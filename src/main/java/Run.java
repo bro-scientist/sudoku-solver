2301c4fbb8f14fc3ef7d58f40b953afb6db697a0
bro-scientist <jacxta@gmail.com>
2021-09-13 14:30:28 +0200

    initial commit

---
+++ src/main/java/Run.java
@@ -0,0 +1,10 @@
+import de.broscientist.sudokusolver.SudokuSolver;
+
+public class Run
+{
+    public static void main(String[] args) {
+        SudokuSolver solver = new SudokuSolver();
+        // solver.init();
+        // solver.solve();
+    }
+}
---
+++ src/main/java/de/broscientist/sudokusolver/SudokuSolver.java
@@ -0,0 +1,9 @@
+package de.broscientist.sudokusolver;
+
+public class SudokuSolver
+{
+    private static final int SIZE = 9;
+    private int[][] grid;
+
+
+}
